d65ded410e347c3097c7b9563d429b4f02e0bf2c
Bophelo Makate <makatebophelo21@gmail.com>
2025-08-22 16:57:20 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println('\n' + "Welcome to Student Grade Analyzer!");
+        System.out.println("1. Add a Student");
+        System.out.println("2. View average grade");
+        System.out.println("3. Find top performer");
+        System.out.println("4. Search student by name");
+        System.out.println("5. Get middle student");
+        System.out.println("6. Exit");
+
+
+    }
+
+
+
+
+
+
+}
